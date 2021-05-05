e804fe029f4bb1c1d285706db9ead7f8bbf7a30c
Emma Dickson <emma.dickson@artsymail.com>
2021-05-04 23:55:02 -0400

    Update azdhs_dashboard.js

--- azdhs_dashboard.js
+++ azdhs_dashboard.js
@@ -12,36 +12,35 @@ module.exports = async ({data, page, crawler}) => {
 
   const gotoOpts = {
     waitUntil: crawler.params.waitUntil,
-    timeout: 0
+    timeout: 2147483647
   };
-  
-  try {
-    await page.goto(url, gotoOpts);
-  } catch (e) {
-    console.log(`Load timeout for ${url}`, e);
-  }
 
   try {
-    await page.goto(url, gotoOpts);
-    requestIndividualDashboards(1)
-    for (i = 3; i <6; i++) {  
+    await page.goto(url, {timeout:2147483647})
+    await requestIndividualDashboards(1)
+    for (i = 3; i <= 16; i++) {  
         await requestIndividualDashboards(i)
     }
-    await page.close()
+    
+    await requestIndividualDashboards(2)
+    for (i = 3; i <= 13; i++) {  
+        await requestIndividualDashboards(i)
+    }
   } 
   catch (e) {
     console.log(`Load timeout for ${url}`, e);
   }
 
+
  async function requestIndividualDashboards(i) {
    console.log("request individual dashboard")
-   await page.waitForSelector('#navigator > div > .menu-icon > .slide-menu-open-box > .fas', {timeout:0})
-   await page.click('#navigator > div > .menu-icon > .slide-menu-open-box > .fas', {timeout:0})
+   await page.waitForSelector('#navigator > div > .menu-icon > .slide-menu-open-box > .fas', {timeout:2147483647})
+   await page.click('#navigator > div > .menu-icon > .slide-menu-open-box > .fas', {timeout:2147483647})
    console.log("side nav bar opened")
-   await page.waitForSelector(`#navigator > div > #sidebar-nav > .list-group-item:nth-child(${i}) > .capturedLink`, {timeout:0})
-   await page.click(`#navigator > div > #sidebar-nav > .list-group-item:nth-child(${i}) > .capturedLink`, {timeout:0})
+   await page.waitForSelector(`#navigator > div > #sidebar-nav > .list-group-item:nth-child(${i}) > .capturedLink`, {timeout:2147483647})
+   await page.click(`#navigator > div > #sidebar-nav > .list-group-item:nth-child(${i}) > .capturedLink`, {timeout:2147483647})
    console.log("clicked")
-   await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"], timeout:0 });
+   await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"], timeout:2147483647 });
    console.log("reloaded")
  }
  
